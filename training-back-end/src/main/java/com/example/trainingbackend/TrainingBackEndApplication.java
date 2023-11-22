06baf830a24c9443f2a8f6e3b9e5b733dfa46c8d
khanhvantg <khanhvanvippro2001@gmail.com>
2023-11-22 09:04:01 +0700

    main add: add project training back end

---
+++ training-back-end/src/main/java/com/example/trainingbackend/TrainingBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBackEndApplication.class, args);
+	}
+
+}
---
+++ training-back-end/src/test/java/com/example/trainingbackend/TrainingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
